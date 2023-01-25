9c9f598d80e8b0596a4e595c26f48c19a92601d2
123RahulRana <104134023+123RahulRana@users.noreply.github.com>
2023-01-25 12:12:07 +0530

    Update creditcardfraud.py
    check the given data is fraud or normal.

--- creditcardfraud.py
+++ creditcardfraud.py
@@ -96,3 +96,22 @@ test_data_accuracy = accuracy_score(x_test_predict, y_test)
 
 print('Accuracy on test data is ', test_data_accuracy)
 
+#check the given data is fraud or normal.
+import pickle
+filename = 'trained_model_froud_detection.sav'
+pickle.dump(model,open(filename, 'wb'))
+
+loaded_model = pickle.load(open(filename, 'rb'))
+
+input = (406,-2.3122265423263,1.95199201064158,-1.60985073229769,3.9979055875468,-0.522187864667764,-1.42654531920595,-2.53738730624579,1.39165724829804,-2.77008927719433,-2.77227214465915,3.20203320709635,-2.89990738849473,-0.595221881324605,-4.28925378244217,0.389724120274487,-1.14074717980657,-2.83005567450437,-0.0168224681808257,0.416955705037907,0.126910559061474,0.517232370861764,-0.0350493686052974,-0.465211076182388,0.320198198514526,0.0445191674731724,0.177839798284401,0.261145002567677,-0.143275874698919,0)
+# change input to m=numpy array
+numpy_input = np.asarray(input)
+
+# reshape array for predicting 1 instance
+input_reshaped = numpy_input.reshape(1,-1)
+
+
+prediction = loaded_model.predict(input_reshaped)
+print(prediction)
+
+
